a5ae25ee266cdfb5046ff1d378572d668fa561f6
paikeee <70816756+paikeee@users.noreply.github.com>
2021-04-01 02:24:00 +0300

    Add files via upload

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,132 +1,98 @@
 package paikeee
 
+
 import java.io.File
 import java.lang.Exception
-
-fun combiner(flagI: Boolean, flagU: Boolean, flagC: Boolean,
-             flagS: Boolean, input: List<String>, s: Int): List<String> = when {
-    flagI && !flagS || flagI && flagS -> register(flagU, flagC, input, s)
-    !flagI && flagS -> sub(flagU, flagC, input, s)
-    else -> simple(flagU, flagC, input)
-}
+import java.lang.IndexOutOfBoundsException
+import java.lang.NumberFormatException
 
 
-fun simple(flagU: Boolean, flagC: Boolean, input: List<String>): List<String> {
+fun combiner(flagI: Boolean, flagU: Boolean, flagS: Boolean,
+             input: List<String>, s: Int): List<Pair<Int, String>> {
 
-    val output = mutableListOf<String>()
-    var last = input.first()
-    var c = 1
+    val output = mutableListOf<Pair<Int, String>>()
+    var last = Pair(1, input.first())
+    var num = 1
 
     for (line in input.subList(1, input.lastIndex + 1)) {
-        if (line != last) {
 
-            if (flagC) {
-                last = "$c" + last
-                c = 1
+        var last1 = if (flagI) last.second.toLowerCase() else last.second
+        var line1 = if (flagI) line.toLowerCase() else line
+        if (flagS) {
+            last1 = last1.substring(s)
+            line1 = line1.substring(s)
+        }
+        if (last1 != line1) {
+            when {
+                flagU && last.first == 1 -> output.add(last)
+                !flagU -> output.add(last)
             }
-            if (flagU && last !in output) output.add(last)
-            if (!flagU) output.add(last)
-
-            last = line
-        } else if (flagC) c++
+            last = Pair(1, line)
+            num = 1
+        } else {
+            num++
+            last = Pair(num, last.second)
+        }
     }
-        if (flagC) last = "$c" + last
-        if (flagU && last !in output) output.add(last)
-        if (!flagU) output.add(last)
+    when {
+        flagU && last.first == 1 -> output.add(last)
+        !flagU -> output.add(last)
+    }
     return output
 }
 
-fun register(flagU: Boolean, flagC: Boolean, input: List<String>, s: Int): List<String> {
 
-    val output = mutableListOf<String>()
-    var last = input.first()
-    var c = 1
+fun main(args: Array<String>) {
 
-    for (line in input.subList(1, input.lastIndex + 1)) {
-        if (line.substring(s).toLowerCase() != last.substring(s).toLowerCase()) {
-
-            if (flagC) {
-                last = "$c" + last
-                c = 1
-            }
-            if (flagU && last !in output) output.add(last)
-            if (!flagU) output.add(last)
-
-            last = line
-        } else if (flagC) c++
-    }
-    if (flagC) last = "$c" + last
-    if (flagU && last !in output) output.add(last)
-    if (!flagU) output.add(last)
-    return output
-}
-
-fun sub(flagU: Boolean, flagC: Boolean, input: List<String>, s: Int): List<String> {
-
-    val output = mutableListOf<String>()
-    var last = input.first()
-    var c = 1
-
-    for (line in input.subList(1, input.lastIndex + 1)) {
-        if (line.substring(s) != last.substring(s)) {
-
-            if (flagC) {
-                last = "$c" + last
-                c = 1
-            }
-            if (flagU && last !in output) output.add(last)
-            if (!flagU) output.add(last)
-
-            last = line
-        } else if (flagC) c++
-    }
-    if (flagC) last = "$c" + last
-    if (flagU && last !in output) output.add(last)
-    if (!flagU) output.add(last)
-    return output
-}
-
-fun main(args: String): List<String> {
-
-    val input = args.split(" ")
     var flagO = false
-    var outputName = String()
+    var outputName = ""
     var flagI = false
     var flagS = false
     var s = 0
     var flagU = false
     var flagC = false
-    var inputName = String()
+    var input = listOf<String>()
 
-    var i = 1
-    while (i < input.size && i <= 8) {
-        when (input[i]) {
+    var i = 0
+    while (i < args.size) {
+        when (args[i]) {
             "-c" -> flagC = true
             "-i" -> flagI = true
             "-u" -> flagU = true
             "-s" -> {
                 flagS = true
-                s = input[i + 1].toInt()
+                try {
+                    if (Regex("""\d*""").matches(args[i + 1])) s = args[i + 1].toInt()
+                    else throw NumberFormatException()
+                } catch (e: IndexOutOfBoundsException)
+                {
+                    throw Exception ("No number for flag -s")
+                }
                 i++
             }
             "-o" -> {
                 flagO = true
-                outputName = input[i + 1]
+                try {
+                    outputName = args[i + 1]
+                } catch (e: IndexOutOfBoundsException)
+                {
+                    throw Exception ("File name expected")
+                }
                 i++
             }
-            else -> if (File(input[i]).isFile) {
-                inputName = input[i]
+            else -> if (File(args[i]).isFile) {
+                input = File(args[i]).readLines()
                 break
-            } else throw Exception("Invalid argument")
+            } else throw Exception("Wrong file name")
         }
         i++
     }
+    if (input.isEmpty())
+        input = (generateSequence { readLine() }.takeWhile{ it != "" }).toList()
 
-    if (inputName.isEmpty()) inputName = readLine() ?: throw Exception("Error")
+    val output = if (flagC) combiner(flagI, flagU, flagS, input, s).map { it.first.toString() + it.second }
+    else combiner(flagI, flagU, flagS, input, s).map { it.second }
 
-    val strings =
-            if (File(inputName).isFile) File(inputName).readLines() else inputName.split("/n")
-    val output = combiner(flagI, flagU, flagC, flagS, strings, s)
     if (flagO)
         File(outputName).bufferedWriter().use { out ->
             output.forEach {
@@ -135,5 +101,4 @@ fun main(args: String): List<String> {
             }
         }
     else for (string in output) println(string)
-    return output
 }
--- src/test/kotlin/testing.kt
+++ src/test/kotlin/testing.kt
@@ -1,17 +1,54 @@
 package paikeee
 
 import org.junit.Test
-import kotlin.test.assertEquals
-
+import org.junit.Assert.*
+import java.lang.Exception
+import java.lang.NumberFormatException
+import java.io.File
 
 class Testing {
+
+    private fun assertFileContent(name: String, expectedContent: String) {
+        val file = File(name)
+        val content = file.readLines().joinToString("\n")
+        assertEquals(expectedContent, content)
+    }
+
     @Test
-    fun mainTest() {
-        assertEquals(listOf("2zzzz", "1SFfdfs", "3eeee"), main("uniq -c -u forTests/1.txt"))
-        assertEquals(listOf("zzzz", "baaaa", "ZzZz"), main("uniq -u -i forTests/2.txt"))
-        assertEquals(listOf("zzzz",  "ZZZZ", "baaaa", "ZzZz"), main("uniq -u -s 3 forTests/2.txt"))
-        assertEquals(listOf("zzzz", "SFfdfs", "eeee"), main("uniq -u forTests/1.txt"))
-        assertEquals(listOf("2zzzz", "1SFfdfs", "3eeee"), main("uniq -c -u -o forTests/result.txt forTests/1.txt"))
-        assertEquals(listOf("2zzzz", "1SFfdfs", "3eeee"), main("uniq -c -u"))
+    fun main() {
+         try {
+              arrayOf("-s")
+         } catch (e: Exception) {
+             println("No number for flag -s")
+         }
+        try {
+            arrayOf("-s dsd")
+        } catch (e: NumberFormatException) {
+            println("Number expected")
+        }
+        try {
+            arrayOf("-o")
+        } catch (e: Exception) {
+            println("File name expected")
+        }
+        try {
+            arrayOf("-i -c qwerty.txxt")
+        } catch (e: Exception) {
+            println("Wrong file name")
+        }
+
+        main(arrayOf("-c", "-o", "forTests/result.txt", "forTests/1.txt"))
+        assertFileContent("forTests/result.txt",
+                "2zzzz\n" +
+                "1wwww\n" +
+                "3eeee")
+        main(arrayOf("-s", "3", "-o", "forTests/result.txt", "forTests/2.txt"))
+        assertFileContent("forTests/result.txt","zzzz\n" +
+                "ZZZZ\n" +
+                "baaaa\n" +
+                "ZzZz")
+        main(arrayOf("-i", "-u", "-o", "forTests/result.txt", "forTests/2.txt"))
+        assertFileContent("forTests/result.txt",
+                "ZzZz")
     }
 }
